01cd7df97b2b885082dc46e9639ede60e07494b1
clntsf <colin.tsf@gmail.com>
2026-02-08 14:47:48 +0000

    updated btf tickers to fit case files

--- src/btf.py
+++ src/btf.py
@@ -6,14 +6,23 @@ from api import TradingAPI
 
 tapi = TradingAPI()
 
-def butterfly(ticker, t1, t2, n=1):
-    tapi.buy(ticker, n, t1)
-    tapi.buy(ticker, n, t2)
-    tapi.sell(ticker, 2*n, (t1+t2)/2)
+def butterfly(t1: int, t2: int, n=1):
+    if (t1 + t2) %2 != 0:
+        print("[ERR] midpoint not whole number, cannot transact!")
+        return
+
+    tickt1 = f"RTM1C{t1}"
+    tickt2 = f"RTM1C{t2}"
+
+    mid = (t1+t2)//2
+    tickmid = f"RTM1P{mid}"
+
+    tapi.buy(tickt1, n, t1)
+    tapi.buy(tickt2, n, t2)
+    tapi.sell(tickmid, 2*n, mid)
 
 if __name__ == "__main__":
-    ticker = argv[1]
-    t1 = float(argv[2])
-    t2 = float(argv[3])
+    t1 = int(argv[1])
+    t2 = int(argv[2])
     n = 1 if len(argv) < 5 else int(argv[4])
-    butterfly(ticker,t1,t2,n)
+    butterfly(t1,t2,n)
